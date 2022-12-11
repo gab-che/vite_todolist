43d76399dc48a9457d3a6773c4787f00a9fa7782
gab-che <gabriellachessa1@gmail.com>
2022-12-11 18:46:48 +0100

    adds title using props

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script>
+  import TheHeader from './components/TheHeader.vue';
+
+  export default{
+    components: { TheHeader },
+  }
 </script>
 
 <template>
-
+  <div class="container main_container my-4">
+    <TheHeader title="Task Tracker"></TheHeader>
+  </div>
 </template>
 
 <style lang="scss">
   @use './styles/general.scss';
-
+  .main_container{
+    border: 1px solid gray;
+    padding: 2rem;
+  }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+    </header>
+</template>
+
+<script>
+    export default{
+        props: {
+            title: String,
+        },
+        components: {},
+    }
+</script>
+
+<style scoped lang="scss">
+    @use '../styles/partials/utilities' as *;
+    @use '../styles/partials/variables' as *;
+
+    header{
+        @include flex_between;
+        margin-bottom: $standard_spacer;
+    }
+
+</style>
